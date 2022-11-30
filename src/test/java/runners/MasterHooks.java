2468b2abaee02744e420b8c50878e5676e604405
walam <walam@arisglobal.com>
2022-11-30 12:19:15 -0500

    PLAT_1408_1348_1320_1645_1352

--- src/test/java/runners/MasterHooks.java
+++ src/test/java/runners/MasterHooks.java
@@ -1,6 +1,22 @@
 package runners;
 
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import utils.CommonUI;
+import utils.ObjInitialize;
+
 public class MasterHooks {
 
+    @Before
+    public void setup() throws Exception {
+        //CommonUI.navigate();
+        //ReadPropertiesFile.setBrowser();
+        ObjInitialize.initializeClassObj();
     }
+    @After
+    public void tearDown(){
+        CommonUI.quitBrowser();
+    }
+}
+
--- src/test/java/utils/CommonUI.java
+++ src/test/java/utils/CommonUI.java
@@ -3,5 +3,30 @@ package utils;
 
 public class CommonUI extends Constants {
 
+    public static void refresh() {
+        try {
+            driver.navigate().refresh();
+        } catch (Exception e) {
+            System.out.println("Webpage refresh failed");
+            e.printStackTrace();
+        }
+    }
 
+    public static void backward() {
+        try {
+            driver.navigate().back();
+        } catch (Exception e) {
+            System.out.println("No previous page to go back to");
+            e.printStackTrace();
+        }
+    }
+
+    public static void forward() {
+        try {
+            driver.navigate().forward();
+        } catch (Exception e) {
+            System.out.println("No next page to go forward to");
+            e.printStackTrace();
+        }
+    }
     }
